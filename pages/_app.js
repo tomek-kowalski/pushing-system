8ca3061dfb72854858cb8e88707145ac82c4f317
Tomasz Kowalski <88949668+tomek-kowalski@users.noreply.github.com>
2025-03-18 14:36:17 +0100

    speed insight

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
+      <SpeedInsights/>
       <Component {...pageProps} />
     </SessionProvider>
   );
